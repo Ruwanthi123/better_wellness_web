5c3627c0ab77ea0a7e3f51411fe8ae84cc762b13
Ruwanthi <ruwanthimunasinghe42@gmail.com>
2025-05-01 14:26:59 +0530

    Change services urls

--- client-service/src/main/angular/src/app/constant/common-settings.ts
+++ client-service/src/main/angular/src/app/constant/common-settings.ts
@@ -1,2 +1,6 @@
-export const MSG_PREFIX = 'http://localhost:8084';
-export const PROF_PREFIX = 'http://localhost:8083';
+// export const MSG_PREFIX = 'http://localhost:8084';
+// export const PROF_PREFIX = 'http://localhost:8083';
+
+
+export const MSG_PREFIX = (window as any)['__env']?.messageApiUrl;
+export const PROF_PREFIX = (window as any)['__env']?.profileApiUrl;
