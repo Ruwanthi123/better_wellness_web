a4638d5cf9b867417082e71b7f88913183a42a1c
Ruwanthi <ruwanthimunasinghe42@gmail.com>
2025-05-01 15:57:56 +0530

    main.yml changes

--- client-service/src/main/angular/src/app/constant/common-settings.ts
+++ client-service/src/main/angular/src/app/constant/common-settings.ts
@@ -2,5 +2,5 @@
 // export const PROF_PREFIX = 'http://localhost:8083';
 
 
-export const MSG_PREFIX = (window as any)['__env']?.messageApiUrl;
-export const PROF_PREFIX = (window as any)['__env']?.profileApiUrl;
+export const MSG_PREFIX = 'http://af615df9e2cf149a187b78cedaff3b3c-105528209.eu-north-1.elb.amazonaws.com'
+export const PROF_PREFIX = 'http://abf303c4440884a539a496bc6b6ef62c-59776705.eu-north-1.elb.amazonaws.com'
