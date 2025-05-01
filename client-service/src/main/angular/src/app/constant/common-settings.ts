72e216fdaf5b728d6737f3dc081a3a650629a08b
Ruwanthi Munasinghe <62273710+Ruwanthi123@users.noreply.github.com>
2025-05-02 02:12:28 +0530

    Update common-settings.ts

--- client-service/src/main/angular/src/app/constant/common-settings.ts
+++ client-service/src/main/angular/src/app/constant/common-settings.ts
@@ -2,5 +2,5 @@
 // export const PROF_PREFIX = 'http://localhost:8083';
 
 
-export const MSG_PREFIX = 'https://d1zzmvs5mescnx.cloudfront.net'
-export const PROF_PREFIX = 'https://d2hrkj2srd1zry.cloudfront.net'
+export const MSG_PREFIX = 'https://d1zzmvs5mescnx.cloudfront.net';
+export const PROF_PREFIX = 'https://d2hrkj2srd1zry.cloudfront.net';
